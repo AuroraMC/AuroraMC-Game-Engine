4e9c0db6f3397803f226a6f3ef83521b3c3d8823
Block2Block <accounts@block2block.me>
2022-02-01 12:43:52 +0000

    Fix wrong pitches

--- src/main/java/net/auroramc/engine/api/util/InGameStartingRunnable.java
+++ src/main/java/net/auroramc/engine/api/util/InGameStartingRunnable.java
@@ -27,7 +27,7 @@ public class InGameStartingRunnable extends BukkitRunnable {
             for (Player player : Bukkit.getOnlinePlayers()) {
                 player.sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Game", String.format("The game is starting in **%s** second%s!", i, ((i > 1)?"s":""))));
                 if (i < 6) {
-                    player.playSound(player.getLocation(), Sound.NOTE_PLING, 100, 5-i);
+                    player.playSound(player.getLocation(), Sound.NOTE_PLING, 100, 3-(i/3f));
                 }
             }
             i--;
@@ -35,7 +35,7 @@ public class InGameStartingRunnable extends BukkitRunnable {
             game.inProgress();
             for (Player player : Bukkit.getOnlinePlayers()) {
                 player.sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Game", "The game has begun!"));
-                player.playSound(player.getLocation(), Sound.NOTE_PLING, 100, 5);
+                player.playSound(player.getLocation(), Sound.NOTE_PLING, 100, 3);
             }
             this.cancel();
         }
