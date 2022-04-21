549b18598b41862c2173832af08770e58c97fd31
Block2Block <accounts@block2block.me>
2022-04-21 23:12:57 +0300

    Games played and games started are now tracked correctly
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/engine/api/games/Game.java
+++ src/main/java/net/auroramc/engine/api/games/Game.java
@@ -17,6 +17,7 @@ import net.auroramc.core.api.players.Team;
 import net.auroramc.core.api.players.scoreboard.PlayerScoreboard;
 import net.auroramc.engine.api.EngineAPI;
 import net.auroramc.engine.api.GameUtils;
+import net.auroramc.engine.api.backend.EngineDatabaseManager;
 import net.auroramc.engine.api.players.AuroraMCGamePlayer;
 import net.auroramc.engine.api.server.ServerState;
 import net.auroramc.engine.api.util.GameStartingRunnable;
@@ -134,6 +135,12 @@ public abstract class Game {
         runnable = new InGameStartingRunnable(this);
         runnable.runTaskTimerAsynchronously(EngineAPI.getGameEngine(), 0, 20);
         gameSession.start();
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                EngineDatabaseManager.gameStarted();
+            }
+        }.runTaskAsynchronously(EngineAPI.getGameEngine());
     }
 
     public void inProgress() {
