2c4e3a5020d092dc967942df709f26061cbba128
Block2Block <accounts@block2block.me>
2022-04-23 00:19:24 +0300

    Disable projectile trails for fishing rods
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/engine/listeners/CosmeticListener.java
+++ src/main/java/net/auroramc/engine/listeners/CosmeticListener.java
@@ -8,6 +8,7 @@ import net.auroramc.core.api.AuroraMCAPI;
 import net.auroramc.core.api.cosmetics.Cosmetic;
 import net.auroramc.core.api.cosmetics.ProjectileTrail;
 import net.auroramc.core.api.players.AuroraMCPlayer;
+import org.bukkit.entity.FishHook;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,7 +18,7 @@ public class CosmeticListener implements Listener {
 
     @EventHandler
     public void onProjectileLaunch(ProjectileLaunchEvent e) {
-        if (e.getEntity().getShooter() instanceof Player) {
+        if (e.getEntity().getShooter() instanceof Player && !(e.getEntity() instanceof FishHook)) {
             AuroraMCPlayer player = AuroraMCAPI.getPlayer((Player)e.getEntity().getShooter());
             if (player.getActiveCosmetics().containsKey(Cosmetic.CosmeticType.PROJECTILE_TRAIL)) {
                 ((ProjectileTrail)player.getActiveCosmetics().get(Cosmetic.CosmeticType.PROJECTILE_TRAIL)).onShoot(e.getEntity());
