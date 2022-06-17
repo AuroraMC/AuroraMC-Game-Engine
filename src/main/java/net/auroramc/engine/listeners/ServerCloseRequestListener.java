cc87610505b1ed057c8205cf16c792ca81b7d947
Block2Block <accounts@block2block.me>
2022-06-17 15:12:43 +0100

    Make updates quicker when no-one is online

--- src/main/java/net/auroramc/engine/listeners/ServerCloseRequestListener.java
+++ src/main/java/net/auroramc/engine/listeners/ServerCloseRequestListener.java
@@ -26,24 +26,30 @@ public class ServerCloseRequestListener implements Listener {
     @EventHandler
     public void onServerCloseRequest(ServerCloseRequestEvent e) {
         if (e.isEmergency() || (EngineAPI.getServerState() != ServerState.IN_GAME && EngineAPI.getServerState() != ServerState.ENDING)) {
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Server Manager", "This server is restarting" + ((!e.isEmergency())?" for an update":"") + ". You are being sent to a lobby."));
-                ByteArrayDataOutput out = ByteStreams.newDataOutput();
-                out.writeUTF("Lobby");
-                out.writeUTF(player.getUniqueId().toString());
-                player.sendPluginMessage(AuroraMCAPI.getCore(), "BungeeCord", out.toByteArray());
-            }
-            //Wait 10 seconds, then close the server
-            new BukkitRunnable(){
-                @Override
-                public void run() {
-                    for (Player player : Bukkit.getOnlinePlayers()) {
-                        player.kickPlayer(AuroraMCAPI.getFormatter().pluginMessage("Server Manager", "This server is restarting.\n\nYou can reconnect to the network to continue playing!"));
-                    }
-                    AuroraMCAPI.setShuttingDown(true);
-                    CommunicationUtils.sendMessage(new ProtocolMessage(Protocol.CONFIRM_SHUTDOWN, "Mission Control", e.getType(), AuroraMCAPI.getServerInfo().getName(), AuroraMCAPI.getServerInfo().getNetwork().name()));
+            if (Bukkit.getOnlinePlayers().size() == 0) {
+                //No-one is online, kill the server now.
+                AuroraMCAPI.setShuttingDown(true);
+                CommunicationUtils.sendMessage(new ProtocolMessage(Protocol.CONFIRM_SHUTDOWN, "Mission Control", e.getType(), AuroraMCAPI.getServerInfo().getName(), AuroraMCAPI.getServerInfo().getNetwork().name()));
+            } else {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Server Manager", "This server is restarting" + ((!e.isEmergency())?" for an update":"") + ". You are being sent to a lobby."));
+                    ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                    out.writeUTF("Lobby");
+                    out.writeUTF(player.getUniqueId().toString());
+                    player.sendPluginMessage(AuroraMCAPI.getCore(), "BungeeCord", out.toByteArray());
                 }
-            }.runTaskLater(AuroraMCAPI.getCore(), 200);
+                //Wait 10 seconds, then close the server
+                new BukkitRunnable(){
+                    @Override
+                    public void run() {
+                        for (Player player : Bukkit.getOnlinePlayers()) {
+                            player.kickPlayer(AuroraMCAPI.getFormatter().pluginMessage("Server Manager", "This server is restarting.\n\nYou can reconnect to the network to continue playing!"));
+                        }
+                        AuroraMCAPI.setShuttingDown(true);
+                        CommunicationUtils.sendMessage(new ProtocolMessage(Protocol.CONFIRM_SHUTDOWN, "Mission Control", e.getType(), AuroraMCAPI.getServerInfo().getName(), AuroraMCAPI.getServerInfo().getNetwork().name()));
+                    }
+                }.runTaskLater(AuroraMCAPI.getCore(), 200);
+            }
         } else {
             //Set that it is awaiting a restart, then restart when the game is over.
             EngineAPI.setAwaitingRestart(true);
